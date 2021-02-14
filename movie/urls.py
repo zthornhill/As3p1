4fa4491166a17a69e778f5c944dbd29ceb27952b
zthornhill <70481420+zthornhill@users.noreply.github.com>
2021-02-14 16:45:49 -0600

    First Commit

---
+++ movie/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+
+from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token
+from api import views
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('auth/', obtain_jwt_token),
+    path('', views.movie_list),
+    url(r'^api/movies/$', views.movie_list),
+    url(r'^api/movies/(?P<pk>[0-9]+)$', views.getMovie),
+]
+
